5fa07c9db077443f7e769b75ec5c31ad0de3b0da
gadajatah <dreamerrr20@gmail.com>
2022-05-24 11:57:08 +0700

    first push '

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $nama_database = "penerimaan_siswa";
+
+    $db = mysqli_connect($server, $user, $password, $nama_database);
+
+    if( !$db ){
+        die("Gagal terhubung dengan database: " . mysqli_connect_error());
+    }
+?>
